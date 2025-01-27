1785aca5ad38a6286334fc7d09efbcd698ff82a7
Waleed Fadl <WwaleedFadl@gmail.com>
2025-01-28 00:43:25 +0200

    hall momkeen

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -79,9 +79,9 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
-
+    const clerk = await clerkClient();
     if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      await clerk.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
